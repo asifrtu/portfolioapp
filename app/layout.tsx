ec22d21ab84d7a66f054e99421cf15f7d8936ab6
MD ASIF RAZA <ASIFRAZARTU@GMAIL.COM>
2026-07-09 17:18:51 +0530

    add some changes on layout here I remove previous image used svg for it in better format

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,9 +21,28 @@ export const metadata: Metadata = {
         media: "(prefers-color-schem*e: dark)",
       },
       {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
+  url:
+    "data:image/svg+xml;utf8," +
+    encodeURIComponent(`
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="32 184 576 272">
+        <rect width="640" height="640" fill="#FFFFFF"/>
+
+        <!-- A -->
+        <rect x="64" y="192" width="64" height="256" fill="#000"/>
+        <rect x="128" y="192" width="128" height="64" fill="#000"/>
+        <rect x="256" y="192" width="64" height="256" fill="#000"/>
+        <rect x="128" y="320" width="128" height="64" fill="#000"/>
+
+        <!-- R -->
+        <rect x="352" y="192" width="64" height="256" fill="#000"/>
+        <rect x="416" y="192" width="96" height="64" fill="#000"/>
+        <rect x="512" y="256" width="64" height="64" fill="#000"/>
+        <rect x="416" y="320" width="96" height="64" fill="#000"/>
+        <rect x="512" y="384" width="64" height="64" fill="#000"/>
+      </svg>
+    `),
+  type: "image",
+}
     ],
     apple: "/apple-icon.png",
   },
